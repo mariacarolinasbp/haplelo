eab08fee0ec387a10dd58a2d1988e2ff21c2a38d
Octávio Richter <4429577+OctavioBR@users.noreply.github.com>
2020-08-17 03:19:33 -0300

    Pregress sample from io.Write()

--- main.go
+++ main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
-	"os"
 	"os/exec"
 )
 
+type Progress struct{}
+
+func (l *Progress) Write(p []byte) (n int, err error) {
+	fmt.Println("out>", string(p))
+	return len(p), nil
+}
+
 func main() {
-	f, err := os.OpenFile("out.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
-	}
-	defer f.Close()
-
 	cmd := exec.Command("phase", "-MS", "-f1", "-S666", "acre.inp", "out/acre", "8000", "300", "1000")
-	log.Printf("Running: %v", cmd.String())
 
-	mwriter := io.MultiWriter(f, os.Stdout)
-	cmd.Stderr = mwriter
-	cmd.Stdout = mwriter
+	var prgs Progress
+	cmd.Stderr = &prgs
 
-	err = cmd.Run()
+	err := cmd.Run()
 	log.Printf("exit error: %v", err)
 }
